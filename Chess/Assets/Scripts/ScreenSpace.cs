0e674d1f3d10d919e024ac177b4aec1c9ae08fdf
CHANI <115384067+ChaniDev@users.noreply.github.com>
2023-01-10 01:47:25 +0530

    Cam Fix-1

--- Chess/Assets/Scripts/ScreenSpace.cs
+++ Chess/Assets/Scripts/ScreenSpace.cs
@@ -9,7 +9,31 @@ public class ScreenSpace : MonoBehaviour
 
     void Start()
     {
+        ScreenSize();
+    }
+
+    void ScreenSize()
+    {
         float camHeight = mainCam.orthographicSize;
         float camWidth = camHeight * mainCam.aspect;
+
+        if(camWidth < 4.15 || camWidth > 4.25 )
+        {
+            FixSize(camWidth);
+        }
+    }
+
+    void FixSize(float cameraWidth)
+    {
+        if(cameraWidth < 4.15)
+        {
+            mainCam.orthographicSize = mainCam.orthographicSize + 0.1f;
+        }
+        else
+        {
+            mainCam.orthographicSize = mainCam.orthographicSize - 0.1f;
+        }
+
+        ScreenSize();
     }
 }
