f1c9a1d58169e72e52fe0bb1539583679c4e276b
CHANI <115384067+ChaniDev@users.noreply.github.com>
2023-01-10 22:37:21 +0530

    Raycast Push 2

--- Chess/Assets/Scripts/BoardManager.cs
+++ Chess/Assets/Scripts/BoardManager.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 
 public class BoardManager : MonoBehaviour
 {
-    
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitData;
+            Physics.Raycast(ray, out hitData);
+        }
+    }
 }
