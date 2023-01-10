6774d941b4ece50ea5a7e0742b2b881311103e7d
CHANI <115384067+ChaniDev@users.noreply.github.com>
2023-01-11 03:12:57 +0530

    Raycast File with hit detection

---
+++ Chess/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    BoardManager insBoardManager; 
+
+    bool WaitForInput = false;
+
+    void Start()
+    {
+        insBoardManager = FindObjectOfType<BoardManager>();
+    }
+
+    public void RequestPlayerInput(int Player) //--Player 1 -(1) \|/ Player 2 -(2)
+    {
+        WaitForInput = true;
+    }
+
+    void Update()
+    {
+        if(WaitForInput == true)
+        {
+            if(Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit rayHit;
+
+                if(Physics.Raycast(ray, out rayHit))
+                {
+                    print("Hit");
+
+                    insBoardManager.SelectedInput
+                        (System.Convert.ToInt16(rayHit.collider.name), rayHit.collider.tag);
+                }
+
+                
+            }
+        }
+    }
+}
