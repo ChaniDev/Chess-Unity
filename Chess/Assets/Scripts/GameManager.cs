d78bbebfb40bf9c33600486bf86d7c09c371d822
CHANI <115384067+ChaniDev@users.noreply.github.com>
2023-01-11 03:12:33 +0530

    Few Mechanical Changes

--- Chess/Assets/Scripts/GameManager.cs
+++ Chess/Assets/Scripts/GameManager.cs
@@ -6,11 +6,13 @@ public class GameManager : MonoBehaviour
 {
     BoardManager insBoardManager;
     GraphicManager insGraphicManager;
+    PlayerManager insPlayerManager;
 
     void Start()
     {
         insBoardManager = FindObjectOfType<BoardManager>();
         insGraphicManager = FindObjectOfType<GraphicManager>();
+        insPlayerManager = FindObjectOfType<PlayerManager>();
 
         StartGame();
     }
@@ -18,6 +20,10 @@ public class GameManager : MonoBehaviour
     void StartGame()
     {
         insGraphicManager.Initiate();
+
+        insPlayerManager.RequestPlayerInput(1);
+
+        
     }
 
 }
