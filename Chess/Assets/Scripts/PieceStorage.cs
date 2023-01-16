29bdee951107eae0b82d90235e431c2c00830aba
CHANI <115384067+ChaniDev@users.noreply.github.com>
2023-01-16 17:53:48 +0530

    New Storage on pieces

--- Chess/Assets/Scripts/PieceStorage.cs
+++ Chess/Assets/Scripts/PieceStorage.cs
@@ -5,9 +5,47 @@ using UnityEngine;
 public class PieceStorage : MonoBehaviour
 {
     [SerializeField] Vector2 piecePosition = new Vector2(0,0);
+    [SerializeField] bool isWhite = false;
+    [SerializeField] bool isInverted = false;
+    [SerializeField] bool isFirstMove = false;
 
-    public Vector2 GetPosition()
+
+        //-- Get Data --
+    public Vector2 GetIndexData()
     {
         return (piecePosition);
     }
+
+    public bool GetColourData()
+    {
+        return (isWhite);
+    }
+
+    public bool GetIfInverted()
+    {
+        return(isInverted);
+    }
+
+    public bool GetIfFirstMove()
+    {
+        return(isFirstMove);
+    }
+
+
+        //-- Set Data --
+    public void SetIndexData(Vector2 indexData)
+    {
+        piecePosition.x = indexData.x;
+        piecePosition.y = indexData.y;
+    }
+
+    public void SetIfInverted(bool ifInverted)
+    {
+        isInverted = ifInverted;
+    }
+
+    public void SetIfFirstMove(bool IfFirstMove)
+    {
+        isFirstMove = IfFirstMove;
+    }  
 }
