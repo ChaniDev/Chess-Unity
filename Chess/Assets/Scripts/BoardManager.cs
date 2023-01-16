fb9d99d2d88c5eb0acb8aee6cd165c5e3d78fd05
CHANI <115384067+ChaniDev@users.noreply.github.com>
2023-01-16 17:53:25 +0530

    Better Board Management

--- Chess/Assets/Scripts/BoardManager.cs
+++ Chess/Assets/Scripts/BoardManager.cs
@@ -6,11 +6,30 @@ public class BoardManager : MonoBehaviour
 {
     GraphicManager insGraphicManager; 
 
-    Vector2[] boardCoordinate = new Vector2[64];
+    Vector2[] boardIndex = new Vector2[64];
+    List<GameObject> pieceStorage = new List<GameObject>(); 
+    List<GameObject> movesStorage = new List<GameObject>();
     
+        // -- Black Pieces --
+    [SerializeField] GameObject whitePawn;
+    [SerializeField] GameObject whiteKnight;
+    [SerializeField] GameObject whiteBishop;
+    [SerializeField] GameObject whiteRook;
+    [SerializeField] GameObject whiteQueen;
+    [SerializeField] GameObject whiteKing;
 
-    int selectedPieceSlot;
-    string selectedPieceName;
+    [Space(20)]
+        // -- Black Pieces --
+    [SerializeField] GameObject blackPawn;
+    [SerializeField] GameObject blackKnight;
+    [SerializeField] GameObject blackBishop;
+    [SerializeField] GameObject blackRook;
+    [SerializeField] GameObject blackQueen;
+    [SerializeField] GameObject blackKing;
+
+    [Space(20)]
+        // -- Move GameObject
+    [SerializeField] GameObject movePreview;
 
 
     void Start()
@@ -28,21 +47,94 @@ public class BoardManager : MonoBehaviour
         {
             for(int x = 0; x < 8; x++)
             {
-                boardCoordinate[slotNumber] = new Vector2(x,y);
+                boardIndex[slotNumber] = new Vector2(x,y);
                 slotNumber++;
             }
         }     
 
         print("Board Indexed");
+
+        InstantiatePiece();
     }
 
-    public void SelectedInput(Vector2 positionPiece, string pieceType)
+    void InstantiatePiece()
     {
-        
+        Vector3 vectorLocation = insGraphicManager.RequestVectorPosition(28);
+            //--Test index 28 --
+        GameObject Pawn = Instantiate(whitePawn, vectorLocation, Quaternion.identity);
+        Pawn.GetComponent<PieceStorage>().SetIndexData(boardIndex[28]);
+        Pawn.GetComponent<PieceStorage>().SetIfInverted(false);
+        Pawn.GetComponent<PieceStorage>().SetIfFirstMove(true);
+        pieceStorage.Add(Pawn);
+    } 
+
+    public void SelectPiece
+        (string pieceType, Vector2 indexPosition, bool isInverted, bool isFirstMove)
+    {
+        if(pieceType.Equals("Board"))
+        {
+
+        }
+        else if(pieceType.Equals("Move"))
+        {
+
+        }
+        else
+        {
+            CalculateMoves(pieceType, indexPosition, isInverted, isFirstMove);
+        }
     }
 
-    void AvailableMoves(string PieceName, Vector2 positionPiece)
+    void DestroyMoves()
     {
-        
+        for(int i = 0; i < movesStorage.Count; i ++)
+        {
+            Destroy(movesStorage[i]);
+        }
+        movesStorage.Clear();
+    }
+
+    void MovePiece()
+    {
+
+    } 
+
+    void CalculateMoves
+        (string pieceType, Vector2 positionPiece, bool isInverted, bool isFirstMove)
+    {
+
+
+        if(pieceType.Equals("Pawn"))
+        {
+            if(!isInverted && isFirstMove)
+            {   
+                Vector2 lastMoveIndex = positionPiece;
+                    //-- Pawn Moves 2 Slots if First Move on that piece (-Inverted-)
+                for(int i = 0; i < 2; i++)
+                {
+                    lastMoveIndex.y++;
+
+                    for(int z = 0; z < boardIndex.Length; z++)
+                    {
+                        if(boardIndex[z].x.Equals(lastMoveIndex.x) &&
+                            (boardIndex[z].y.Equals(lastMoveIndex.y)))
+                        {
+                            Vector3 vectorPos = insGraphicManager.RequestVectorPosition(z);
+
+                            
+                        }
+                    }
+                }
+            }
+            else if(!isInverted && !isFirstMove)
+            {
+
+            }
+        }
+    }
+
+    void CheckIfLegal(List<move>)
+    {
+
     }
 }
