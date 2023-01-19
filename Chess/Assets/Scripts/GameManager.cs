05186c29070c2d5e2102235659aa5bd785131a92
CHANI <115384067+ChaniDev@users.noreply.github.com>
2023-01-20 03:59:42 +0530

    Update GameManager.cs

--- Chess/Assets/Scripts/GameManager.cs
+++ Chess/Assets/Scripts/GameManager.cs
@@ -8,20 +8,56 @@ public class GameManager : MonoBehaviour
     GraphicManager insGraphicManager;
     PlayerManager insPlayerManager;
 
+    [SerializeField] GameObject settingGame; 
+    [SerializeField] bool smartAI = true;
+
+    int? gameMode = null; // -- gamemode 1 = PVE // ---gamemode 2 = PVP
+    int? colourSelected = null;  // -- colourSelected 1 = White // ---colourSelected 2 = Black
+    
+
+
+
     void Start()
     {
         insBoardManager = FindObjectOfType<BoardManager>();
         insGraphicManager = FindObjectOfType<GraphicManager>();
         insPlayerManager = FindObjectOfType<PlayerManager>();
-
-        StartGame();
     }
 
     void StartGame()
     {
-        insGraphicManager.Initiate();
+        settingGame.SetActive(false);
 
+        insGraphicManager.Initiate();
         insPlayerManager.RequestPlayerInput(1);
+        insBoardManager.StartGame((int)colourSelected);
     }
 
+    public void LaunchGame()
+    {
+        if(gameMode != null || colourSelected != null)
+        {
+            StartGame();
+        }
+    }
+
+    public void SelectedWhite()
+    {
+        colourSelected = 1;
+    }
+
+    public void SelectedBlack()
+    {
+        colourSelected = 2;
+    }
+
+    public void GameModePVE()
+    {
+        gameMode = 1;
+    }
+
+    public void GameModePVP()
+    {
+        gameMode = 2;
+    }
 }
